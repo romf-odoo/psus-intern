fd275c893aa20600fdcd9d250a107051d2387d39
Roberto Merino Flores <robert@pop-os.localdomain>
2022-06-07 16:40:45 -0500

    Important points about python course

--- essentials.py
+++ essentials.py
@@ -1,112 +1,111 @@
-# print("hello word", 4.5)
+print("hello word", 4.5)
 # # variables
-# hello = "tim"
-# word = "word"
-# word = hellohello = "no"
-# print(hello, word)
-# # convencion variable
-# hello_word = "hola"
+hello = "tim"
+word = "word"
+word = hellohello = "no"
+print(hello, word)
+# convencion variable
+hello_word = "hola"
 # # input
-# name = input('Name: ')
-# print(name)
+name = input('Name: ')
+print(name)
 # # operaciones aritmeticas 
 # # convert to integer
-# x = 9 
-# y = 3
-# int(x/y)
+x = 9 
+y = 3
+int(x/y)
 
 # # exponent
 
-# x**y
+x**y
 # # floor division
 
-# x//y
+x//y
 
-# #mod
+#mod
 
-# x%3
+x%3
 
-# #modular  operation
+#modular  operation
 
-# (x%y) * 2
+(x%y) * 2
 
-# num = input ("Number: ")
-# print(int(num)-5)
-
-
-
-# #type func
-
-
-# hello = "hello"
-# print(type(hello))
-
-# #string methods
-
-
-# hello = hello.upper()
-# print(hello)
-
-
-# hello = hello.lower()
-
-# hello.capitalize()
-# #counts the amount of coincidences found
-# print(hello.lower().count("ll"))
+num = input ("Number: ")
+print(int(num)-5)
 
 
 
-# #conditions
-# x = 7
-# y = 8
-# z = 0
-# result1 = x == y
-# result2 = y > x
-# result3 = z < x + 2
+#type func
 
-# result4 = result1 or result2
 
-# x = "tim"
+hello = "hello"
+print(type(hello))
 
-# if x == "tim":
-#     print("tim is great")
-# elif x == "bob":
-#     print("bob is great")
-# else:
-#     print("You are not tim")
+#string methods
+
+
+hello = hello.upper()
+print(hello)
+
+
+hello = hello.lower()
+
+hello.capitalize()
+#counts the amount of coincidences found
+print(hello.lower().count("ll"))
+
+
+
+#conditions
+x = 7
+y = 8
+z = 0
+result1 = x == y
+result2 = y > x
+result3 = z < x + 2
+
+result4 = result1 or result2
+
+x = "tim"
+
+if x == "tim":
+    print("tim is great")
+elif x == "bob":
+    print("bob is great")
+else:
+    print("You are not tim")
 
 
 #list are mutable objects
+x = [4, True, "hi"]
+x.append("hello")
+print(x)
+x.extend([1,2,3,4,5,6,7])
 
-# x = [4, True, "hi"]
-# x.append("hello")
-# print(x)
-# x.extend([1,2,3,4,5,6,7])
-
-# print(x.pop(0))
-
-
-# print(x[1])
-
-# #tuples are immutable
-
-# y = (0,0,2,2)
+print(x.pop(0))
 
 
-# #loops
+print(x[1])
 
-# for i in range(10):
-#     print("hello")
-# i = 0
-# while i < 10:
-#     print("hello1")
-#     i+=1  
-# i = 0
-# while True:
-#     print("hello2 ", i)
-#     i += 1
-#     if i == 10:
-#         break   
+#tuples are immutable
+
+y = (0,0,2,2)
+
+
+#loops
+
+for i in range(10):
+   print("hello")
+i = 0
+while i < 10:
+    print("hello1")
+    i+=1  
+i = 0
+while True:
+    print("hello2 ", i)
+    i += 1
+    if i == 10:
+        break   
 #slice operator
 x = [0,1,2,3,4,5,6,7,8]    
 sliced = x[0:4:2]
@@ -119,12 +118,12 @@ s = "hello world"
 sliced = s[::-1]    
 print(sliced)
 #sets
-# x = set()
-# s = {4,32,2,2}
+x = set()
+s = {4,32,2,2}
 
-# print(s)
+print(s)
 
-# print(2 in s)
+print(2 in s)
 
 #dictionaries
 x = {'key':4}
